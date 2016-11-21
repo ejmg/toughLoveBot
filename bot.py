fc96a69743500fbb688ed024e24f129751e0f21a
ejmg <elias.jm.garcia@gmail.com>
2016-11-21 16:51:00 -0600

    init files

---
+++ bot.py
@@ -0,0 +1,23 @@
+"""
+a bot that tweets tough love (and some non-sense humor)
+
+author: elias g. and elliot p.
+version: 21.11.16
+"""
+import tweepy as ty
+from toughLoveSecret import (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN,
+                             ACCESS_SECRET)
+
+
+def setTwitterAuth():
+    """
+    obtains authorization from twitter API
+    """
+    # sets the auth tokens for twitter using tweepy
+    auth = ty.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = ty.API(auth)
+    return api
+
+if __name__ == "__main__":
+    api = setTwitterAuth()
---
+++ toughLoveTweets.py
@@ -0,0 +1,8 @@
+"""
+tweets for ToughLoveBot by elias g. and elliot p. These tweets are by elliot p.
+with minimal editing by elias g.
+
+authors: elias g. and elliot p.
+version: 21.11.16
+"""
+toughLove ={}
