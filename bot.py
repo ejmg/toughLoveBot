710adc1c3e8b6c4ad391170b152ba4b74780d047
ejmg <elias.jm.garcia@gmail.com>
2016-11-22 10:30:36 -0600

    testing api configs, bot not ready yet

--- bot.py
+++ bot.py
@@ -5,8 +5,16 @@ author: elias g. and elliot p.
 version: 21.11.16
 """
 import tweepy as ty
+from toughLoveTweets import (tweets, worriedReplies, lonelyReplies,
+                             sickReplies)
 from toughLoveSecret import (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN,
                              ACCESS_SECRET)
+
+# testing out api stuff before moving on...
+def getLast20Tweets(api):
+    last20 = api.user_timeline()
+    for oldtweet in last20:
+        print(oldtweet.text)
 
 
 def setTwitterAuth():
@@ -19,7 +27,12 @@ def setTwitterAuth():
     api = ty.API(auth)
     return api
 
+# driver is testing out stuff rn, not for deployment
 if __name__ == "__main__":
     api = setTwitterAuth()
-    tweet = ""
-    api.update_status(tweet)
+    me = api.me()
+    print(dir(me))
+    print(me.id)
+    getLast20Tweets(api)
+    # tweet = ""
+    # api.update_status()
